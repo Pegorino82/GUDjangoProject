5b5df56b19020c8d78330ff47cb957cb36519aeb
Pegorino82 <eshkryabin@yandex.ru>
2018-10-03 12:25:43 +0300

    made necessary models

--- myshop/main/models.py
+++ myshop/main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=150)
+    lastname = models.CharField(max_length=150)
+
+    def __str__(self):
+        return '{} {}'.format(self.name, self.lastname)
+
+    def __repr__(self):
+        return '{} {}'.format(self.name, self.lastname)
+
+
+class MainPageContent(models.Model):
+    chapter = models.CharField(max_length=150)
+    content = models.CharField(max_length=500)
+    date = models.DateTimeField()
+    author = models.ForeignKey(
+        'main.Author',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return '{} {} (author {})'.format(self.chapter[:10],
+                                          self.content[:10],
+                                          self.author)
--- myshop/products/models.py
+++ myshop/products/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
+
+
+class ProductMarker(models.Model):
+    corner = models.CharField(max_length=45,
+                              null=True,
+                              blank=True)
+
+    def __str__(self):
+        return self.corner
+
+
+class Image(models.Model):
+    '''переделать на работу с медиафайлами'''
+    img = models.CharField(max_length=250,
+                           null=True,
+                           blank=True)
+
+    def __str__(self):
+        if self.img:
+            return self.img.split('/')[-1]
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    short_text = models.CharField(max_length=150)
+    long_text = models.CharField(max_length=350)
+    now_price = models.DecimalField(max_digits=10, decimal_places=2)
+    old_price = models.DecimalField(max_digits=10,
+                                    decimal_places=2,
+                                    null=True,
+                                    blank=True)
+    product_marker = models.ForeignKey(
+        'products.ProductMarker',
+        on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        'products.Category',
+        on_delete=models.CASCADE
+    )
+    image = models.ForeignKey(
+        'products.Image',
+        on_delete=models.CASCADE
+    )
