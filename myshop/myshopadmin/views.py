88a3f985cc5a60df44bf9ebbc3ea586c81928f9e
Pegorino82 <eshkryabin@yandex.ru>
2018-10-13 11:42:36 +0300

    fixed static

--- myshop/myshopadmin/views.py
+++ myshop/myshopadmin/views.py
@@ -7,31 +7,31 @@ from products.models import Product
 
 class ModelCreateProduct(CreateView):
     model = Product
-    fields = []
+    fields = ['name', 'short_text', 'image', 'now_price', 'old_price', 'category']
     template_name = 'myshopadmin/create.html'
     success_url = reverse_lazy('products:catalog')
 
 
 class ModelUpdateProduct(UpdateView):
     model = Product
-    fields = []
+    fields = ['name', 'short_text', 'long_text', 'image', 'now_price', 'old_price', 'category', 'product_marker']
     template_name = 'myshopadmin/update.html'
     success_url = reverse_lazy('products:catalog')
 
 class ModelDeleteProduct(DeleteView):
     model = Product
-    fields = []
+    fields = ['name', 'short_text', 'long_text', 'image', 'now_price', 'old_price', 'category', 'product_marker']
     template_name = 'myshopadmin/delete.html'
     success_url = reverse_lazy('products:catalog')
 
 class ModelDetailProduct(DetailView):
     model = Product
-    fields = []
+    fields = ['name', 'short_text', 'long_text', 'image', 'now_price', 'old_price', 'category', 'product_marker']
     template_name = 'myshopadmin/detail.html'
     success_url = reverse_lazy('products:catalog')
 
 class ModelListProduct(ListView):
     model = Product
-    fields = []
+    fields = ['name', 'short_text', 'image', 'now_price', 'old_price', 'category', 'product_marker']
     template_name = 'myshopadmin/list.html'
     success_url = reverse_lazy('products:catalog')
