8eafaf985b9cd32de0ae99f8cd53ee6a84da9078
Pegorino82 <eshkryabin@yandex.ru>
2018-10-14 19:28:55 +0300

    fix bug with register new user (customer)

--- myshop/customers/views.py
+++ myshop/customers/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user
+from django.db.utils import IntegrityError
 
 from .models import Customer
 
@@ -29,25 +30,36 @@ def login_view(request):
             logout(request)
 
         if request.POST.get('logup'):
+            # print('*' * 100)
+            # print('logup-->', request.POST)
+            # print('logup-->', request.session)
+            # print('*' * 100)
 
-            checker = Customer.objects.get(username=usr_name)
-            if not checker:
+            checker = get_user(request)
 
-                customer = Customer(
-                    username=usr_name
-                )
-                customer.is_active = True
-                customer.set_password(psw)
-                customer.save()
+            if checker.is_anonymous:
 
-                notification = {
-                    'user_name': usr_name,
-                    'warn': 'Congratulations!'
-                }
-            else:
-                notification = {
-                    'user_name': usr_name,
-                    'warn': 'Name is occupied! Choose another'
-                }
+                try:
+                    customer = Customer(
+                        username=usr_name
+                    )
+                    customer.is_active = True
+                    customer.set_password(psw)
+                    customer.save()
+
+                    notification = {
+                        'user_name': usr_name,
+                        'warn': 'Congratulations!'
+                    }
+                except IntegrityError:
+                    notification = {
+                        'user_name': usr_name,
+                        'warn': 'Name is occupied! Choose another'
+                    }
+            # else:
+            #     notification = {
+            #         'user_name': usr_name,
+            #         'warn': 'Name is occupied! Choose another'
+            #     }
 
     return render(request, 'customers/customer.html', notification)
