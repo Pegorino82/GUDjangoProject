0da8736cc6374c954ef7016bd80807e63a1a9bc5
Pegorino82 <eshkryabin@yandex.ru>
2018-11-02 10:39:14 +0300

    minor

--- myshop/basket/static/basket/js/basket_scripts.js
+++ myshop/basket/static/basket/js/basket_scripts.js
@@ -1,19 +1,38 @@
-window.onload = function () {
-    $('.product-line').on('click', 'a', function () {
-        var t_href = event.target;
+// window.onload = function () {
+//     $('.product-line').on('click', 'a', function () {
+//         var t_href = event.target;
+//         console.log(t_href.href);
+//
+//         $('')
+//
+//         $.ajax({
+//             url: "basket/edit" + t_href.href,
+//             success: function (data) {
+//                 $('.product-line').html(data.result);
+//             },
+//         });
+//
+//         event.preventDefault();
+//     });
+// }
 
-        $.ajax({
-            url: "/basket/basket/",
 
-            success: function (data) {
-                $('.product-line').html(data.result);
-            },
-        });
-
-        event.preventDefault();
-    });
-}
-
+//Foo = window.onload = function ({quantity, product_id}) {
+//    $('.product-line').on('click', 'a', function () {
+//        var t_href = event.target;
+//        console.log(quantity, product_id);
+//
+//        $.ajax({
+//            url: "edit/" + product_id + "/" + quantity + "/",
+//            success: function (data) {
+//                $('.product-line').html(data.result);
+//            },
+//        });
+//
+//        event.preventDefault();
+//    });
+//}
+//
 const Basket = ({quantity}) => (
     `
     ${ quantity }
@@ -22,7 +41,9 @@ const Basket = ({quantity}) => (
 
 const renderData = res => {
     console.log(res.data.results);
-    itm = res.data.results.map(Basket);
+//    res.data.results.map(Foo);
+    itm = res.data.results.map(Basket).join(' / ');
     basket_js = document.getElementById('basket_js');
     basket_js.innerHTML = itm;
 }
+
