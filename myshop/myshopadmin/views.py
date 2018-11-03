b7fbc20dc57d72e5b0427003cbaa3ca24168bc05
Pegorino82 <eshkryabin@yandex.ru>
2018-11-03 14:44:45 +0300

    rename controllers; protected with LoginRequiredMixin, UserPassesTestMixin

--- myshop/myshopadmin/views.py
+++ myshop/myshopadmin/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.apps import apps
 from collections import OrderedDict
 
 from products.models import Product
 
 
-class ModelCreateProduct(CreateView):
+class ModelCreate(LoginRequiredMixin, CreateView):
     template_name = 'myshopadmin/create.html'
+    success_url = 'customersapp:customer'
 
     # def get_context_data(self, **kwargs):
     #     '''not used here'''
@@ -22,7 +25,7 @@ class ModelCreateProduct(CreateView):
         model = kwargs.get('model')
         self.model = apps.get_model(app, model.title())
         self.fields = [field.name for field in self.model._meta.__dict__.get('local_fields') if field.editable]
-        return super(ModelCreateProduct, self).get(request, *args, **kwargs)
+        return super(ModelCreate, self).get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         app = kwargs.get('app')
@@ -30,11 +33,12 @@ class ModelCreateProduct(CreateView):
         self.model = apps.get_model(app, model.title())
         self.fields = [field.name for field in self.model._meta.__dict__.get('local_fields') if field.editable]
         self.success_url = reverse_lazy('myshopadminapp:list', kwargs=kwargs)
-        return super(ModelCreateProduct, self).post(request)
+        return super(ModelCreate, self).post(request)
 
 
-class ModelUpdateProduct(UpdateView):
+class ModelUpdate(LoginRequiredMixin, UpdateView):
     template_name = 'myshopadmin/update.html'
+    success_url = 'customersapp:customer'
 
     def get(self, request, *args, **kwargs):
         app = kwargs.get('app')
@@ -43,7 +47,7 @@ class ModelUpdateProduct(UpdateView):
         self.model = apps.get_model(app, model.title())
         self.object = self.model.objects.get(pk=pk)
         self.fields = [field.name for field in self.model._meta.__dict__.get('local_fields') if field.editable]
-        return super(ModelUpdateProduct, self).get(request, **kwargs)
+        return super(ModelUpdate, self).get(request, **kwargs)
 
     def post(self, request, *args, **kwargs):
         app = kwargs.get('app')
@@ -53,11 +57,12 @@ class ModelUpdateProduct(UpdateView):
         self.object = self.model.objects.get(pk=pk)
         self.fields = [field.name for field in self.model._meta.__dict__.get('local_fields') if field.editable]
         self.success_url = reverse_lazy('myshopadminapp:detail', kwargs=kwargs)
-        return super(ModelUpdateProduct, self).post(request)
+        return super(ModelUpdate, self).post(request)
 
 
-class ModelDeleteProduct(DeleteView):
+class ModelDelete(LoginRequiredMixin, DeleteView):
     template_name = 'myshopadmin/delete.html'
+    success_url = 'customersapp:customer'
 
     def get(self, request, *args, **kwargs):
         app = kwargs.get('app')
@@ -66,7 +71,7 @@ class ModelDeleteProduct(DeleteView):
         self.model = apps.get_model(app, model.title())
         self.object = self.model.objects.get(pk=pk)
         self.fields = [field.name for field in self.model._meta.__dict__.get('local_fields') if field.editable]
-        return super(ModelDeleteProduct, self).get(request, **kwargs)
+        return super(ModelDelete, self).get(request, **kwargs)
 
     def post(self, request, *args, **kwargs):
         app = kwargs.get('app')
@@ -74,12 +79,13 @@ class ModelDeleteProduct(DeleteView):
         self.model = apps.get_model(app, model.title())
         self.fields = [field.name for field in self.model._meta.__dict__.get('local_fields') if field.editable]
         self.success_url = reverse_lazy('myshopadminapp:list', kwargs={'app': app, 'model': model})
-        return super(ModelDeleteProduct, self).post(request)
+        return super(ModelDelete, self).post(request)
 
 
-class ModelDetailProduct(DetailView):
+class ModelDetail(LoginRequiredMixin, DetailView):
     template_name = 'myshopadmin/detail.html'
     context_object_name = 'results'
+    success_url = 'customersapp:customer'
 
     def get(self, request, *args, **kwargs):
         self._app = kwargs.get('app')
@@ -88,34 +94,36 @@ class ModelDetailProduct(DetailView):
         self.model = apps.get_model(self._app, self._model.title())
         self.object = self.model.objects.get(pk=self._pk)
         self.fields = [field.name for field in self.model._meta.__dict__.get('local_fields') if field.editable]
-        return super(ModelDetailProduct, self).get(request, **kwargs)
+        return super(ModelDetail, self).get(request, **kwargs)
 
     def get_context_data(self, **kwargs):
-        context = super(ModelDetailProduct, self).get_context_data(**kwargs)
+        context = super(ModelDetail, self).get_context_data(**kwargs)
         context['app'] = self._app
         context['model'] = self._model
         context['pk'] = self._pk
         return context
 
 
-class ModelListProduct(ListView):
+class ModelList(LoginRequiredMixin, ListView):
     template_name = 'myshopadmin/list.html'
     context_object_name = 'results'
+    success_url = 'customersapp:customer'
 
     def get(self, request, *args, **kwargs):
         self._app = kwargs.get('app')
         self._model = kwargs.get('model')
         self.model = apps.get_model(self._app, self._model.title())
         self.fields = [field.name for field in self.model._meta.__dict__.get('local_fields') if field.editable]
-        return super(ModelListProduct, self).get(request, *args, **kwargs)
+        return super(ModelList, self).get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
-        context = super(ModelListProduct, self).get_context_data(**kwargs)
+        context = super(ModelList, self).get_context_data(**kwargs)
         context['app'] = self._app
         context['model'] = self._model
         return context
 
 
+@login_required(login_url='customersapp:customer')
 def index(request):
     all_apps = apps.get_models()
 
@@ -131,11 +139,11 @@ def index(request):
     # context = {'results': prep_dict}
     context = {
         'results': OrderedDict(sorted({
-            'products': ('Product', 'Category', 'ProductMarker'),
-            'images': ('Image',),
-            'customers': ('Customer',),
-            'main': ('Author', 'MainPageContent')
-        }.items()))
+                                          'products': ('Product', 'Category', 'ProductMarker'),
+                                          'images': ('Image',),
+                                          'customers': ('Customer',),
+                                          'main': ('Author', 'MainPageContent')
+                                      }.items()))
     }
 
     return render(request, 'myshopadmin/index.html', context)
