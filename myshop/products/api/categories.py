789a236ab6eeee7832257fa3d856b4591d77b502
Pegorino82 <eshkryabin@yandex.ru>
2018-11-09 22:52:21 +0300

    pagitation for api

--- myshop/products/api/categories.py
+++ myshop/products/api/categories.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
-
+from django.shortcuts import reverse, render, redirect, get_object_or_404, get_list_or_404
+from django.core.paginator import Paginator
 from django.http import JsonResponse
 
 from products.models import Category
@@ -7,16 +7,32 @@ from products.models import Category
 
 def rest_category_list(request):
     query_set = get_list_or_404(Category)
-    data = [i.__dict__ for i in query_set]
-    for d in data:
-        d.pop('_state')
-        d.pop('id')
-
-    return JsonResponse(
-        {
-            'results': data
-        }
+    quantity_per_page = request.GET.get('quantity_per_page') if request.GET.get('quantity_per_page') else len(query_set)
+    paginator = Paginator(query_set, quantity_per_page)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    rout_url = reverse('rest_products:rest_list')
+    data = list(
+        map(
+            lambda itm: {
+                'name': itm.name,
+                'short_text': itm.short_text[:50] + '...' if (
+                        itm.short_text and len(itm.short_text) > 50) else itm.short_text,
+            },
+            page
+        )
     )
+
+    result = {
+        'next_url': f'{rout_url}?page={page.next_page_number()}' if page.has_next() else None,
+        'previous_url': f'{rout_url}?page={page.previous_page_number()}' if page.has_previous() else None,
+        'page': paginator.num_pages,
+        'count': paginator.count,
+        'results': data
+    }
+
+    return JsonResponse(result)
+
 
 def rest_category_detail(request, **kwargs):
     pk = kwargs.get('pk')
--- myshop/products/api/products.py
+++ myshop/products/api/products.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.shortcuts import reverse, render, redirect, get_object_or_404, get_list_or_404
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.core.paginator import Paginator
 
 from products.models import Product, ProductMarker, Category
 from images.models import Image
@@ -45,12 +46,17 @@ def rest_product_create(request):
 
 
 def rest_product_list(request):
+    quantity_per_page = request.GET.get('quantity_per_page') if  request.GET.get('quantity_per_page') else 10
     query_set = get_list_or_404(Product)
+    paginator = Paginator(query_set, quantity_per_page)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    rout_url = reverse('rest_products:rest_list')
     data = list(
         map(
             lambda itm: {
                 'name': itm.name,
-                'short_text': itm.short_text[:50] + '...' if len(itm.short_text) > 50 else itm.short_text,
+                'short_text': itm.short_text[:50] + '...' if (itm.short_text and len(itm.short_text) > 50) else itm.short_text,
                 'now_price': itm.now_price,
                 'old_price': itm.old_price,
                 'currency': itm.currency,
@@ -58,31 +64,36 @@ def rest_product_list(request):
                 'category': itm.category.name,
                 'image': itm.image.img.url
             },
-            query_set
+            page
         )
     )
 
-    return JsonResponse(
-        {
-            'results': data
-        }
-    )
+    result = {
+        'next_url': f'{rout_url}?page={page.next_page_number()}' if page.has_next() else None,
+        'previous_url': f'{rout_url}?page={page.previous_page_number()}' if page.has_previous() else None,
+        'page': paginator.num_pages,
+        'count': paginator.count,
+        'results': data
+    }
+
+    return JsonResponse(result)
+
 
 
 def rest_product_detail(request, **kwargs):
     pk = kwargs.get('pk')
     obj = get_object_or_404(Product, id=pk)
     data = {
-                'name': obj.name,
-                'short_text': obj.short_text[:50] + '...' if len(obj.short_text) > 50 else obj.short_text,
-                'long_text': obj.short_text[:50] + '...' if len(obj.long_text) > 50 else obj.long_text,
-                'now_price': obj.now_price,
-                'old_price': obj.old_price,
-                'currency': obj.currency,
-                'product_marker': obj.product_marker.corner,
-                'category': obj.category.name,
-                'image': obj.image.img.url
-            }
+        'name': obj.name,
+        'short_text': obj.short_text[:50] + '...' if len(obj.short_text) > 50 else obj.short_text,
+        'long_text': obj.short_text[:50] + '...' if len(obj.long_text) > 50 else obj.long_text,
+        'now_price': obj.now_price,
+        'old_price': obj.old_price,
+        'currency': obj.currency,
+        'product_marker': obj.product_marker.corner,
+        'category': obj.category.name,
+        'image': obj.image.img.url
+    }
 
     return JsonResponse(
         {
@@ -108,6 +119,7 @@ def rest_product_update(request, **kwargs):
             'results': data
         }
     )
+
 
 def rest_product_delete(request, **kwargs):
     pk = kwargs.get('pk')
